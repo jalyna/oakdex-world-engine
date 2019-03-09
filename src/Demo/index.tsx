56a29feb488ce717c1f85c872fdb7bc883825fb8
Jalyna <jalyna.schroeder@gmail.com>
2019-03-09 19:56:46 +0100

    Allow other chars to move

--- src/Demo/index.tsx
+++ src/Demo/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import WorldEngine, { Direction } from '..'
+import WorldEngine, { Direction, ActionHandler } from '..'
 
 import * as charset1 from './charset1.png'
 import * as charset2 from './charset2.png'
@@ -9,6 +9,41 @@ import * as charset3 from './charset3.png'
 import * as charset4 from './charset4.png'
 
 const mapData = require('./demo.gamemap.json')
+
+let actionHandler: ActionHandler
+
+function timeout (ms: number): Promise<undefined> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function onEnterStranger () {
+  console.log('TRIGGERED STRANGER')
+  if (!actionHandler) {
+    return
+  }
+  actionHandler.disableMovement()
+  await actionHandler.changeCharDir('stranger-woman', Direction.Left)
+  await timeout(300)
+  await actionHandler.changeCharDir('stranger-woman', Direction.Down)
+  await timeout(600)
+  await actionHandler.changeCharDir('stranger-woman', Direction.Right)
+  await timeout(600)
+  const up1 = await actionHandler.moveChar('stranger-woman', Direction.Up, { msPerFrame: 100 })
+  const up2 = await actionHandler.moveChar('stranger-woman', Direction.Up)
+  if (up1) {
+    await actionHandler.moveChar('stranger-woman', Direction.Down, { msPerFrame: 150 })
+  }
+  if (up2) {
+    await actionHandler.moveChar('stranger-woman', Direction.Down)
+  }
+  const left = await actionHandler.moveChar('stranger-woman', Direction.Left)
+  
+  if (left) {
+    await actionHandler.moveChar('stranger-woman', Direction.Right)
+  }
+  actionHandler.enableMovement()
+  console.log('DONE')
+}
 
 ReactDOM.render(
   <WorldEngine
@@ -20,7 +55,8 @@ ReactDOM.render(
       { id: 'guy', image: charset3, x: 44, y: 12, lookNotInDirection: true },
       { id: 'umbrella-woman', image: charset4, x: 40, y: 20, dir: Direction.Left, walkThrough: true }
     ]}
-    onPressEnter={(charId, triggeredChar) => console.log('Pressing Enter For', triggeredChar.id)}
+    onLoaded={(mapActionHandler) => { actionHandler = mapActionHandler }}
+    onPressEnter={(charId, triggeredChar) => triggeredChar.id === 'stranger-woman' ? onEnterStranger() : console.log('Pressing Enter For', triggeredChar.id)}
     onOver={(charId, triggeredChar) => console.log('Walked over', triggeredChar.id)}
     onWalksTo={(charId, { prev, next }) => console.log(charId, 'walked to', next.x, next.y, next.looksAt.x, next.looksAt.y, next.special)}
     />,
--- src/index.tsx
+++ src/index.tsx
@@ -65,6 +65,17 @@ export interface Viewport {
   height: number
 }
 
+interface MoveCharOptions {
+  msPerFrame?: number
+}
+
+export interface ActionHandler {
+  moveChar: (charId: string, dir: Direction, options?: MoveCharOptions) => Promise<boolean>,
+  changeCharDir: (charId: string, dir: Direction) => Promise<undefined>,
+  disableMovement: () => void,
+  enableMovement: () => void
+}
+
 export interface WorldEngineProps {
   mapData: MapData,
   viewport: Viewport,
@@ -72,7 +83,8 @@ export interface WorldEngineProps {
   chars: Char[],
   onWalksTo?: (charId: string, change: CoordinateChange) => void,
   onPressEnter?: (charId: string, triggeredChar: CharState) => void,
-  onOver?: (charId: string, triggeredChar: CharState) => void
+  onOver?: (charId: string, triggeredChar: CharState) => void,
+  onLoaded?: (actionHandler: ActionHandler) => void
 }
 
 export interface CharState extends Coordinates {
@@ -89,7 +101,8 @@ export interface CharState extends Coordinates {
 export interface WorldEngineState {
   chars: CharState[],
   pressedKey: Direction | null,
-  otherPressedKeys: Direction[]
+  otherPressedKeys: Direction[],
+  disabledMovement?: boolean
 }
 
 export const TILE_SIZE = 16
@@ -105,6 +118,10 @@ function getInitialCharState (char: Char): CharState {
   }
 }
 
+function timeout (ms: number): Promise<undefined> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 export default class WorldEngine extends React.Component<WorldEngineProps, WorldEngineState> {
   constructor (props: WorldEngineProps) {
     super(props)
@@ -116,10 +133,13 @@ export default class WorldEngine extends React.Component<WorldEngineProps, World
     this.onKeyDown = this.onKeyDown.bind(this)
     this.onKeyUp = this.onKeyUp.bind(this)
     this.tick = this.tick.bind(this)
+    this.moveChar = this.moveChar.bind(this)
+    this.changeCharDir = this.changeCharDir.bind(this)
   }
 
   private canvas = React.createRef<HTMLCanvasElement>()
   private interval: number | null
+  private actionHandler: ActionHandler
 
   render () {
     const { top, left } = calculateViewport(this.props.viewport, this.props.mapData, this.controllableChar)
@@ -232,11 +252,54 @@ export default class WorldEngine extends React.Component<WorldEngineProps, World
     })
   }
 
+  changeCharDir (charId: string, dir: Direction): Promise<undefined> {
+    return new Promise((resolve) => {
+      this.changeChar(charId, { dir })
+      setTimeout(resolve, FRAME_DURATION)
+    })
+  }
+
+  moveChar (charId: string, dir: Direction, options?: MoveCharOptions): Promise<boolean> {
+    options = options || {}
+    return new Promise(async(resolve) => {
+      const char = this.state.chars.find((c) => c.id === charId)
+      if (!char) {
+        resolve(false)
+        return
+      }
+      if (char.dir !== dir) {
+        await this.changeCharDir(charId, dir)
+      }
+
+      if (!this.isNextFieldWalkable(charId)) {
+        resolve(false)
+        return
+      }
+
+      for (let i = 1; i < FRAMES_PER_STEP; i++) {
+        this.changeChar(charId, { progressFrame: i, animationFrame: i })
+        await timeout(options.msPerFrame || FRAME_DURATION)
+      }
+      this.finishStep(char.id)
+      await timeout(options.msPerFrame || FRAME_DURATION)
+      resolve(true)
+    })
+  }
+
   componentDidMount () {
     this.redraw()
     document.addEventListener('keydown', this.onKeyDown)
     document.addEventListener('keyup', this.onKeyUp)
     this.interval = setInterval(this.tick, FRAME_DURATION)
+    this.actionHandler = {
+      moveChar: this.moveChar,
+      changeCharDir: this.changeCharDir,
+      disableMovement: () => this.setState({ disabledMovement: true }),
+      enableMovement: () => this.setState({ disabledMovement: false })
+    }
+    if (this.props.onLoaded) {
+      this.props.onLoaded(this.actionHandler)
+    }
   }
 
   componentWillUnmount () {
@@ -302,17 +365,17 @@ export default class WorldEngine extends React.Component<WorldEngineProps, World
     this.interval = null
   }
 
-  triggerOnWalksTo (oldCoordinates: Coordinates) {
+  triggerOnWalksTo (charId: string, oldCoordinates: Coordinates) {
     if (this.props.onOver) {
       const overChar = this.state.chars.find((c) => c.id !== this.controllableChar.id && c.x === this.controllableChar.x && c.y === this.controllableChar.y)
       if (overChar) {
-        this.props.onOver(this.controllableChar.id, { ...overChar })
+        this.props.onOver(charId, { ...overChar })
       }
     }
     if (!this.props.onWalksTo) {
       return
     }
-    this.props.onWalksTo(this.controllableChar.id, {
+    this.props.onWalksTo(charId, {
       prev: {
         ...oldCoordinates,
         special: this.props.mapData.specialTiles[oldCoordinates.y][oldCoordinates.x] || null 
@@ -326,18 +389,26 @@ export default class WorldEngine extends React.Component<WorldEngineProps, World
     })
   }
 
-  finishStep () {
-    const { x, y } = this.getNextCoordinates()
-    const prevX = this.controllableChar.x
-    const prevY = this.controllableChar.y
-    this.changeControllableChar({ x, y, progressFrame: 0, animationFrame: 0 })
-    this.triggerOnWalksTo({ x: prevX, y: prevY })
+  finishStep (charId?: string) {
+    let char = this.controllableChar
+    if (charId) {
+      char = this.state.chars.find((c) => c.id === charId)
+    }
+    const { x, y } = this.getNextCoordinates(char.id)
+    const prevX = char.x
+    const prevY = char.y
+    this.changeChar(char.id, { x, y, progressFrame: 0, animationFrame: 0 })
+    this.triggerOnWalksTo(char.id, { x: prevX, y: prevY })
   }
 
-  getNextCoordinates (): Coordinates {
-    let x = this.controllableChar.x
-    let y = this.controllableChar.y
-    switch (this.controllableChar.dir) {
+  getNextCoordinates (charId?: string): Coordinates {
+    let char = this.controllableChar
+    if (charId) {
+      char = this.state.chars.find((c) => c.id === charId)
+    }
+    let x = char.x
+    let y = char.y
+    switch (char.dir) {
       case Direction.Left:
         x = x - 1
         break
@@ -380,8 +451,8 @@ export default class WorldEngine extends React.Component<WorldEngineProps, World
     return this.props.mapData.walkability[y][x]
   }
 
-  isNextFieldWalkable (): boolean {
-    const { x, y } = this.getNextCoordinates()
+  isNextFieldWalkable (charId: string): boolean {
+    const { x, y } = this.getNextCoordinates(charId)
     const fieldData = this.getFieldData(x, y)
     switch (this.controllableChar.dir) {
       case Direction.Left:
@@ -411,14 +482,18 @@ export default class WorldEngine extends React.Component<WorldEngineProps, World
       this.clearAnimation()
       return
     }
+    // Forbid Movement
+    if (this.state.disabledMovement) {
+      return
+    }
     // First press => change dir only
     if (this.controllableChar.dir !== this.state.pressedKey) {
       this.changeControllableChar({ dir: this.state.pressedKey })
-      this.triggerOnWalksTo({ x: this.controllableChar.x, y: this.controllableChar.y })
+      this.triggerOnWalksTo(this.controllableChar.id, { x: this.controllableChar.x, y: this.controllableChar.y })
       return
     }
     // Can not walk to next field
-    if (!this.isNextFieldWalkable()) {
+    if (!this.isNextFieldWalkable(this.controllableChar.id)) {
       this.changeControllableChar({
         animationFrame: 1
       })
