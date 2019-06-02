cbf98623b285f1d7a754bba35ea65b764f652a8e
Jalyna <jalyna.schroeder@gmail.com>
2019-06-02 16:20:49 +0200

    fix walkability issue

--- src/isNextFieldWalkable.ts
+++ src/isNextFieldWalkable.ts
@@ -20,15 +20,16 @@ export function isFieldWalkable (mapData: MapData, chars: CharState[], charId: s
   const otherChars = chars.filter((c) => c.id !== charId)
   const fieldData = getFieldData(mapData, otherChars, x, y)
   const char = chars.find((c) => c.id === charId)
+  const currentFieldData = getFieldData(mapData, otherChars, char.x, char.y)
   switch (char.dir) {
     case Direction.Left:
-      return fieldData.right === 0
+      return fieldData.right === 0 && currentFieldData.left === 0
     case Direction.Right:
-      return fieldData.left === 0
+      return fieldData.left === 0 && currentFieldData.right === 0
     case Direction.Down:
-      return fieldData.top === 0
+      return fieldData.top === 0 && currentFieldData.bottom === 0
     case Direction.Up:
-      return fieldData.bottom === 0
+      return fieldData.bottom === 0 && currentFieldData.top === 0
   }
   return false
 }
